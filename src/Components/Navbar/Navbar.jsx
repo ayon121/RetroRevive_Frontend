0598c456b67ead0000cc092afd7c47563b9e0e4c
ayon121 <borshonsaha1234@gmail.com>
2024-03-26 12:03:08 -0700

    banner added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,9 +8,12 @@ import { AuthContext } from '../../Providers/Authproviders';
 import { IoGameController } from "react-icons/io5";
 import useSingleUser from '../../Hooks/useSingleUser';
 
+// logo
+import logo from '../../assets/RetroLogo.png'
+
 const Navbar = () => {
     const { user, logout } = useContext(AuthContext)
-    const [singleuserInfo , refetch] = useSingleUser()
+    const [singleuserInfo, refetch] = useSingleUser()
     const handlelogOut = () => {
         logout()
             .then()
@@ -23,7 +26,7 @@ const Navbar = () => {
         <li><NavLink to="/games">Games</NavLink></li>
         {
             singleuserInfo[0]?.userRole === 'admin' && <div>
-               <li><NavLink to="/dashboard">Dashboard</NavLink></li>
+                <li><NavLink to="/dashboard">Dashboard</NavLink></li>
             </div>
         }
 
@@ -33,40 +36,38 @@ const Navbar = () => {
     </>
 
     return (
-        <div className="navbar max-w-7xl mx-auto font-Hind px-1 md:px-3">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <div className=''>
+            <div className="navbar max-w-7xl mx-auto font-Hind px-1 md:px-3 fixed ">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {navlinks}
+                        </ul>
+                    </div>
+                    {/* logo */}
+                    <div className="font-Hind font-bold ">
+                        <img className='h-16 w-28' src={logo} alt="Retro Revive" />
+                    </div>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-2 font-bold">
                         {navlinks}
                     </ul>
                 </div>
-                {/* logo */}
-                <div className="font-Hind font-bold ">
-                    <h1 className=" flex items-center gap-1">
-                        <span className='text-xl md:text-3xl lg:text-4xl'>GRT</span>
-                        <span className='text-lg  md:text-2xl lg:text-3xl text-[#3fb43b]'><IoGameController /></span>
-                        <span className="text-sm md:text-lg lg:text-xl">Reviews</span>
-                    </h1>
+                <div className="navbar-end">
+                    {
+                        user ? <button onClick={handlelogOut} className="btn bg-[#403F3F] text-white hover:text-black">Logout</button>
+                            :
+                            <Link to="/login">
+                                <button className="btn btn-outline btn-success">Login</button>
+                            </Link>
+                    }
+
+
                 </div>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 gap-2 font-bold">
-                    {navlinks}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                {
-                    user ? <button onClick={handlelogOut} className="btn bg-[#403F3F] text-white hover:text-black">Logout</button>
-                        :
-                        <Link to="/login">
-                            <button className="btn btn-outline btn-success">Login</button>
-                        </Link>
-                }
-
-
             </div>
         </div>
     );
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Helmet } from "react-helmet";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
@@ -9,6 +10,10 @@ const Home = () => {
             <title>GRT | Home</title>
         </Helmet>
             <Navbar></Navbar>
+            {/* body */}
+            <div className="min-h-screen">
+                <Banner></Banner>
+            </div>
             <Footer>
             </Footer>
         </div>
