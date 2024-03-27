7acc45b318857b766b3a08f6f23693482e66288f
ayon121 <borshonsaha1234@gmail.com>
2024-03-27 11:20:52 -0700

    navbar upadated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../Providers/Authproviders';
 
-import { IoGameController } from "react-icons/io5";
 import useSingleUser from '../../Hooks/useSingleUser';
 
 // logo
@@ -36,39 +35,41 @@ const Navbar = () => {
     </>
 
     return (
-        <div className=''>
-            <div className="navbar max-w-7xl mx-auto font-Hind px-1 md:px-3 fixed ">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <div className='w-4/5'>
+            <div className='w-full bg-gradient-to-r from-pink-500/40  via-purple-500 to-sky-300/70 fixed lg:rounded-b-3xl border-b-2 border-sky-800'>
+                <div className="navbar font-Hind px-1 md:px-3 max-w-7xl mx-auto">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navlinks}
+                            </ul>
+                        </div>
+                        {/* logo */}
+                        <div className="font-Hind font-bold ">
+                            <img className='h-16 w-28' src={logo} alt="Retro Revive" />
+                        </div>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1 gap-2 font-bold">
                             {navlinks}
                         </ul>
                     </div>
-                    {/* logo */}
-                    <div className="font-Hind font-bold ">
-                        <img className='h-16 w-28' src={logo} alt="Retro Revive" />
+                    <div className="navbar-end">
+                        {
+                            user ? <button onClick={handlelogOut} className="btn bg-[#403F3F] text-white hover:text-black">Logout</button>
+                                :
+                                <Link to="/login">
+                                    <button className="btn btn-outline btn-success">Login</button>
+                                </Link>
+                        }
+
+
                     </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 gap-2 font-bold">
-                        {navlinks}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ? <button onClick={handlelogOut} className="btn bg-[#403F3F] text-white hover:text-black">Logout</button>
-                            :
-                            <Link to="/login">
-                                <button className="btn btn-outline btn-success">Login</button>
-                            </Link>
-                    }
-
-
-                </div>
-            </div>
+            </div> 
         </div>
     );
 };
