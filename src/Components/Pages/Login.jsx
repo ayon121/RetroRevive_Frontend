765b71bff2c37b9b272faaedf796452dc939a632
ayon121 <borshonsaha1234@gmail.com>
2024-03-29 12:11:03 -0700

    login page added

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -68,27 +68,27 @@ const Login = () => {
                 <title>RETRO REVIVE | Login</title>
             </Helmet>
             <Navbar></Navbar>
-            <div className="flex  justify-center w-full h-screen items-center font-Hind px-3">
+            <div className="flex  justify-center w-full h-screen items-center font-Hind px-3 mt-7">
                 <div className=" max-w-4xl mx-auto rounded-2xl flex h-96 py-8 px-3 mt-11" >
                     {/* login form */}
-                    <div className="flex flex-col  py-1 px-8  border-l-4 border-[#3fb43b] h-full">
+                    <div className="flex flex-col  py-1 px-8  border-l-4 border-pink-500 h-full">
                         <form className='flex flex-col' onSubmit={handlelogin}>
-                            <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-[#3fb43b]">Login Now</h1>
-                            <input className="py-1 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="email" name="email" placeholder="email" />
-                            <input className="py-1 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="password" name="password" placeholder="password" />
-                            <input className="btn border-2 border-[#3fb43b] hover:bg-[#3fb43b] hover:text-white" type="submit" value="Login" />
+                            <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-pink-500">Login Now</h1>
+                            <input className="py-1 px-3  my-2 border-2 border-black hover:border-pink-500 rounded-lg" type="email" name="email" placeholder="email" />
+                            <input className="py-1 px-3  my-2 border-2 border-black hover:border-pink-500 rounded-lg" type="password" name="password" placeholder="password" />
+                            <input className='btn bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600 w-full' type="submit" value="Login" />
 
                         </form>
                         <div className="text-center ">
-                            <p className="text-[#3fb43b] pb-1">or login using google</p>
-                            <button onClick={HandleGoogleLogin} className="btn border-2 border-[#3fb43b] w-full hover:bg-[#3fb43b] hover:text-white"><FcGoogle></FcGoogle> Google</button>
-                            <p className='pt-2 '>Do not have a account? <Link to="/register"> <button className='text-[#3fb43b] underline'>Register Now</button></Link></p>
+                            <p className="text-sky-400 pb-1">or login using google</p>
+                            <button onClick={HandleGoogleLogin} className='btn bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600 w-full'><FcGoogle></FcGoogle> Google</button>
+                            <p className='pt-2 text-sky-400'>Do not have a account? <Link to="/register"> <button className='text-pink-500 underline'>Register Now</button></Link></p>
 
                         </div>
                     </div>
                     {/* wishes */}
-                    <div className=" bg-[#3fb43b] rounded-r-2xl hidden md:flex">
-                        <img className='h-full w-full rounded-r-2xl' src={loginimg} alt="hotel img" />
+                    <div className=" bg-pink-500 rounded-r-2xl hidden md:flex">
+                        <img className='h-full w-full rounded-r-2xl' src={loginimg} alt="login img" />
 
                     </div>
                 </div>
