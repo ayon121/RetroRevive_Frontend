a687188b97d8e656667284c2c83cd985494c121f
ayon121 <borshonsaha1234@gmail.com>
2024-03-29 10:43:32 -0700

    marque added

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,15 +1,20 @@
 import Title from "../Title/Title";
 import Product from "./Product";
-
+import Marquee from "react-fast-marquee";
 const Products = () => {
     return (
         <div>
-            <Title title={'Latest Products'}></Title>
-            <div className="grid grid-cols-3 gap-4 px-3 max-w-6xl mx-auto mb-4 md:mb-6">
-                <Product></Product>
-                <Product></Product>
-                <Product></Product>
-            </div>
+            <Title title={'Latest Gaming Products'}></Title>
+            <Marquee>
+                <div className="grid grid-cols-5 gap-4 px-3 max-w-6xl mx-auto mb-4 md:mb-6">
+                    <Product></Product>
+                    <Product></Product>
+                    <Product></Product>
+                    <Product></Product>
+                    <Product></Product>
+                </div>
+            </Marquee>
+            
         </div>
     );
 };
