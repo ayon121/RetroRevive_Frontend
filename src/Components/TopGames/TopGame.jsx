140a9aca970926ec054ed8e219c6608b72350945
ayon121 <borshonsaha1234@gmail.com>
2024-04-01 12:57:20 -0700

    top games funtion completed

--- src/Components/TopGames/TopGame.jsx
+++ src/Components/TopGames/TopGame.jsx
@@ -1,20 +1,28 @@
 
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { FaHeart } from "react-icons/fa";
-const TopGame = () => {
+import { useNavigate } from 'react-router-dom';
+const TopGame = ({topgame}) => {
+    let {title , img , description , likes} = topgame
+
+    if (description.length > 100) {
+        description = description.substring(0, 120) + "...";
+    }
+
+    const navigate = useNavigate()
     return (
-        <div className="card w-auto text-white shadow-2xl  mb-3 border-2  bg-gradient-to-r from-slate-900/50   to-sky-950/70  lg:rounded-b-3xl border-b-2 border-sky-300 duration-500 hover:top-3 ">
-            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="games" /></figure>
+        <div onClick={() => navigate('/games')} className="card w-auto text-white shadow-2xl  mb-3 border-2  bg-gradient-to-r from-slate-900/50   to-sky-950/70  lg:rounded-b-3xl border-b-2 border-sky-300 duration-500 hover:top-3 ">
+            <figure><img src={img} alt="games" /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    TIC TAC TOE
+                    {title}
                     <div className="badge badge-secondary">TOP</div>
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>{description}</p>
                 <div className="card-actions justify-end">
                     <div className='flex items-center gap-2 text-lg '>
                         <FaHeart  className='text-red-400 hover:text-red-700'/>
-                        <h2 className="font-semibold text-white">10k</h2>
+                        <h2 className="font-semibold text-white">{likes}</h2>
                     </div>
                 </div>
             </div>
@@ -23,7 +31,7 @@ const TopGame = () => {
 };
 
 TopGame.propTypes = {
-
+    topgame : PropTypes.object
 };
 
 export default TopGame;
--- src/Components/TopGames/TopGames.jsx
+++ src/Components/TopGames/TopGames.jsx
@@ -1,15 +1,23 @@
+import { useEffect, useState } from "react";
 import Title from "../Title/Title";
 import TopGame from "./TopGame";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 
 const TopGames = () => {
+    const [topgames, Settopgames] = useState([])
+    const axiosSecure = useAxiosSecure()
+    useEffect(() => {
+        axiosSecure.get('/topgames')
+            .then(res => Settopgames(res.data))
+    }, [axiosSecure])
     return (
         <div>
             <Title title={'Top Retro Games'}></Title>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 px-3 max-w-6xl mx-auto mb-4">
-            <TopGame></TopGame>
-            <TopGame></TopGame>
-            <TopGame></TopGame>
+                {
+                    topgames.map(topgame => <TopGame  key={topgame._id} topgame={topgame}></TopGame>)
+                }
             </div>
         </div>
     );
