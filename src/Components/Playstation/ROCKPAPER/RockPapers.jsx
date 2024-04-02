6042cf72fa99c332267c9aca10600eed745714fd
ayon121 <borshonsaha1234@gmail.com>
2024-04-02 13:31:29 -0700

    rock paper function completed

--- src/Components/Playstation/ROCKPAPER/RockPapers.jsx
+++ src/Components/Playstation/ROCKPAPER/RockPapers.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../../Footer/Footer";
 import Navbar from "../../Navbar/Navbar";
 import "../../PageCSS/Home.css"
@@ -10,11 +10,44 @@ const RockPapers = () => {
     const [computerChoice , SetComputerChoice ] = useState('Rock')
     const [winner , SetWinner] = useState(null)
     const [gameOver , SetgameOver] =useState(false)
+    const [userpoints , Setuserpoints] =useState(0)
+    const [computerPoints , SetcomputerPoints] =useState(0)
     const choices = ["Rock", "paper", "scissor"]
 
     const handleChoice = (choice) => {
         SetuserChoice(choice)
+        GanerateComputerChoice()
     }
+
+    const GanerateComputerChoice = () => {
+        const RandomChoice = choices[Math.floor(Math.random() * choices.length)]
+        SetComputerChoice(RandomChoice)
+    }
+
+    useEffect(()=> {
+        const comboMoves = userChoice + computerChoice
+        if(userpoints <= 4 && computerPoints <=4){
+            if(comboMoves === "Rockscissor" || comboMoves === "paperRock" || comboMoves === "scissorpaper"){
+                const updateUserPoints= userpoints + 1
+                Setuserpoints(updateUserPoints)
+                if(updateUserPoints === 5){
+                    SetWinner('You Won the Game')
+                }
+            }
+            if(comboMoves === "paperscissor" || comboMoves === "scissorRock" || comboMoves === "Rockpaper"){
+                const updateComputerPoints= computerPoints + 1
+                SetcomputerPoints(updateComputerPoints)
+                if(updateComputerPoints === 5){
+                    SetWinner('Computer Won the Game')
+                }
+
+            }
+            if(comboMoves === "paperpaper" || comboMoves === "scissorscissor" || comboMoves === "RockRock"){
+                SetWinner('Stay Playing')
+            }
+        }
+
+    },[userChoice , computerChoice])
     return (
         <div className="backgrnd ">
             <Navbar></Navbar>
@@ -36,6 +69,16 @@ const RockPapers = () => {
 
                     </div>
                      {/* --------------Game-------------------------- */}
+
+                     {/* ----------points---------- */}
+                     <div className="text-sky-400 px-4 text-center mb-4 ">
+                        <h1 className="text-2xl md:text-3xl font-extrabold ">Points</h1>
+                        <div className="flex gap-3 justify-center font-bold items-center">
+                            <h1>You : {userpoints}</h1> vs
+                            <h1>Com : {computerPoints}</h1>
+                        </div>
+                     </div>
+                     {/* ----------------------btns-------------- */}
                     <div className="flex justify-center gap-3">
                         {/* rock btn */}
                         <button onClick={() => handleChoice('Rock')} className='btn bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600'>ROCK</button>
@@ -46,11 +89,19 @@ const RockPapers = () => {
                     </div>
                     {/* winner */}
                     <div className="flex justify-center px-2 my-5">
-                        <h1 className="text-3xl md:text-4xl text-white font-extrabold font-Rubik">Winner</h1>
+                        <h1 className="text-3xl md:text-4xl text-white font-extrabold font-Rubik">{winner}</h1>
                     </div>
                     <div className="flex justify-center gap-3">
                         {/* start btn */}
-                        <button className='btn w-3/4 px-4 mt-5 mb-3 bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600'>START</button>
+                        {
+                            (userpoints === 5 || computerPoints === 5) &&
+                                <button onClick={() => {
+                                    SetcomputerPoints(0)
+                                     Setuserpoints(0)
+                                     SetWinner(null)
+                                    }} className='btn w-3/4 px-4 mt-5 mb-3 bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600'>RESTART</button>
+                            
+                        }
                      
                     </div>
 
