37396348afb059eed6a1b7ed9aeb9bb17290f474
ayon121 <borshonsaha1234@gmail.com>
2024-04-05 04:00:35 -0700

    edit review page updated

--- src/Components/Review/ReviewEdit.jsx
+++ src/Components/Review/ReviewEdit.jsx
@@ -4,7 +4,7 @@ import loginimg from '../../assets/login1.jpg'
 import Navbar from '../Navbar/Navbar';
 import { useParams } from 'react-router-dom';
 import useAxiosSecure from '../../Hooks/useAxiosSecure';
-
+import "../PageCSS/Home.css"
 const ReviewEdit = () => {
     const { id } = useParams()
     const axiosSecure = useAxiosSecure()
@@ -24,17 +24,17 @@ const ReviewEdit = () => {
     }
     
     return (
-        <div>
+        <div className='backgrnd'>
         <Navbar></Navbar>
         <div className="flex  justify-center w-full h-screen items-center font-Hind px-3">
             <div className=" max-w-4xl mx-auto rounded-2xl flex h-96 py-8 px-3 mt-11" >
                 {/* login form */}
-                <div className="flex flex-row gap-4  py-1 px-8  border-l-4 border-[#3fb43b] h-full">
+                <div className="flex flex-row gap-4  py-1 px-8  border-l-4 border-pink-500 h-full">
                     <form className='flex flex-col' onSubmit={handleReviewEdit}>
-                        <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-[#3fb43b]">Give Review </h1>
-                        <input className="py-1 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="text" name="number" placeholder="give 1 to 5 review" />
-                        <input className="py-3 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="text" name="message" placeholder="message" />
-                        <input className="btn border-2 border-[#3fb43b] hover:bg-[#3fb43b] hover:text-white" type="submit" value="Give Review" />
+                        <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-white">Edit Review </h1>
+                        <input className="py-1 px-3  my-2 border-2 border-black hover:border-pink-500 rounded-lg" type="text" name="number" placeholder="give 1 to 5 review" />
+                        <input className="py-3 px-3  my-2 border-2 border-black hover:border-pink-500 rounded-lg" type="text" name="message" placeholder="message" />
+                        <input className='btn bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600 w-full' type="submit" value="Edit Review" />
 
                     </form>
                 {/* wishes */}
