47d07fed084832af45f32a703a7bd6bbc5f53a02
ayon121 <borshonsaha1234@gmail.com>
2024-04-06 07:34:31 -0700

    add new game

--- src/Components/Dashboard/AddGame.jsx
+++ src/Components/Dashboard/AddGame.jsx
@@ -2,6 +2,7 @@
 // import PropTypes from 'prop-types';
 
 import { ToastContainer, toast } from "react-toastify";
+import Title from "../Title/Title";
 
 const AddGame = () => {
     const handleAddGame = e => {
@@ -33,18 +34,18 @@ const AddGame = () => {
     }
     return (
         <div>
-            <h1 className="text-center px-3 text-4xl md:text-5xl uppercase mt-5 mb-4 font-mono border-4 rounded-3xl border-black max-w-3xl mx-auto">Add a new game</h1>
+            <Title title={'Add New Game'}></Title>
             <div>
                 <div className="flex  justify-center w-full h-screen font-Hind px-3">
                     <div className=" max-w-4xl mx-auto rounded-2xl flex h-96 py-8 px-3" >
                         {/* login form */}
-                        <div className="flex flex-row gap-4  py-1 px-28  border-x-4 rounded-3xl border-[#3fb43b] h-full">
+                        <div className="flex flex-row gap-4  py-1 px-28  border-x-4 rounded-3xl border-sky-300 h-full">
                             <form className='flex flex-col' onSubmit={handleAddGame}>
-                                <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-[#3fb43b]">Write Game Details </h1>
-                                <input className="py-1 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="title" name="title" placeholder="title" />
-                                <input className="py-1 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="text" name="img" placeholder="game img url" />
-                                <input className="py-3 px-3  my-2 border-2 border-[#3fb43b] rounded-lg" type="text" name="description" placeholder="game description" />
-                                <input className="btn border-2 border-[#3fb43b] hover:bg-[#3fb43b] hover:text-white" type="submit" value="ADD GAME" />
+                                <h1 className=" text-3xl md:text-2xl lg:text-3xl font-bold mt-4 mb-2 text-sky-300">Write Game Details </h1>
+                                <input className="py-1 px-3  my-2 border-2 border-sky-200 hover:border-pink-600 rounded-lg" type="title" name="title" placeholder="title" />
+                                <input className="py-1 px-3  my-2 border-2 border-sky-200 hover:border-pink-600 rounded-lg" type="text" name="img" placeholder="game img url" />
+                                <input className="py-3 px-3  my-2 border-2 border-sky-200 hover:border-pink-600 rounded-lg" type="text" name="description" placeholder="game description" />
+                                <input  className='btn bg-white text-black font-bold hover:text-pink-600 border-black hover:border-pink-600' type="submit" value="ADD GAME" />
 
                             </form>
 
