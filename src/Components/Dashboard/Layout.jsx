40feb80d7163b406f0e7adf8aab3f528a54aece8
ayon121 <borshonsaha1234@gmail.com>
2024-04-06 05:57:39 -0700

    dashboard sidebar

--- src/Components/Dashboard/Layout.jsx
+++ src/Components/Dashboard/Layout.jsx
@@ -6,13 +6,13 @@ const Layout = () => {
     return (
         <div className="backgrnd">
             <div className="flex gap-4 ">
-                <div className=" px-4 border-r-2 border-sky-300 flex flex-col pt-5 justify-center bg-gradient-to-r from-pink-800/40  via-purple-950 to-sky-950/80">
-                    <img className="w-full h-40 px-3" src={cat} alt="cat" />
-                    <Link> <button className="btn w-full  my-3">Dashboard</button></Link>
-                    <Link><button className="btn  w-full my-3">Add Game</button></Link>
-                    <Link><button className="btn w-full my-3">Add Product</button></Link>
-                    <Link><button className="btn w-full my-3">Delete Reviews</button></Link>
-                    <Link><button className="btn w-full my-3">Home</button></Link>
+                <div className="lg:w-64 md:w-44 px-3 border-r-2 border-sky-300 flex flex-col pt-5 justify-center bg-gradient-to-r from-pink-800/40  via-purple-950 to-sky-950/80">
+                    <img className="w-full  h-40" src={cat} alt="cat" />
+                    <Link> <button className="btn w-full   my-3 bg-gradient-to-r from-sky-700/40  via-purple-500/40 to-pink-400/80 font-bold hover:border-sky-300">Dashboard</button></Link>
+                    <Link><button className="btn w-full   my-3 bg-gradient-to-r from-sky-700/40  via-purple-500/40 to-pink-400/80 font-bold hover:border-sky-300">Add Game</button></Link>
+                    <Link><button className="btn w-full   my-3 bg-gradient-to-r from-sky-700/40  via-purple-500/40 to-pink-400/80 font-bold hover:border-sky-300">Add Product</button></Link>
+                    <Link to="/dashboard/deletereviews"><button className="btn w-full   my-3 bg-gradient-to-r from-sky-700/40  via-purple-500/40 to-pink-400/80 font-bold hover:border-sky-300">Delete Reviews</button></Link>
+                    <Link><button className="btn w-full   my-3 bg-gradient-to-r from-sky-700/40  via-purple-500/40 to-pink-400/80 font-bold hover:border-sky-300">Home</button></Link>
                     {/* <div role="tablist" className="tabs tabs-boxed text-center text-[#3fb43b] font-bold gap-3 ">
                     <Link to="/dashboard/addgame"><a role="tab" className="tab bg-[#3fb43b] text-white px-7">ADD GAME</a></Link>
                     <Link to="/dashboard/deletereviews"><a role="tab" className="tab bg-[#3fb43b] text-white px-7">DELETE REVIEWS</a></Link>
